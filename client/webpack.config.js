a74de6bcae6c92953029d9e84f9c4d6dde91abc8
Ian Senne <ian@iansenne.com>
2023-02-28 11:04:39 -0800

    start webpack config for client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,52 @@
-const path = require('path');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require("path");
+const path = require("path");
+const { GenerateSW } = require("workbox-webpack-plugin");
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
-    },
+	return {
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+			cards: "./src/js/cards.js",
+		},
 
-    // TODO: Add the correct output
-    output: {
-      
-    },
+		// TODO: Add the correct output
+		output: {
+			filename: "[name].bundle.js",
+			path: resolve(__dirname, "dist"),
+		},
 
-    // TODO: Add the correct plugins
-    plugins: [
-     
-    ],
+		// TODO: Add the correct plugins
+		plugins: [
+			new GenerateSW({
+				// more configuration here.
+			}),
+			new HtmlWebpackPlugin(),
+		],
 
-    // TODO: Add the correct modules
-    module: {
-
-    }
-  };
+		// TODO: Add the correct modules
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-object-rest-spread",
+								"@babel/transform-runtime",
+							],
+						},
+					},
+				},
+			],
+		},
+	};
 };
